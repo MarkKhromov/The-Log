1588e9c752f39edcd50844793cd7a5be13afd988
Mark Khromov <lane.71rus@gmail.com>
2017-04-01 09:46:05 +0300

    Added base project files

---
+++ TheLog/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("The Log")]
+[assembly: AssemblyProduct("Th eLog")]
+[assembly: AssemblyCopyright("Copyright © Mark Khromov 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("968ef723-0186-4600-9c3b-674f480f7333")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
