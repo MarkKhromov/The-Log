25bbecacc0664076bb06feb8077fbbdfc7e3f0bb
Mark Khromov <lane.71rus@gmail.com>
2017-04-02 12:12:17 +0300

    Added missing assembly info

---
+++ TheLog.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TheLog.Tests")]
+[assembly: AssemblyProduct("TheLog.Tests")]
+[assembly: AssemblyCopyright("Copyright © Mark Khromov 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("11de09ee-338c-4989-a2ae-d0565d84cf91")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
