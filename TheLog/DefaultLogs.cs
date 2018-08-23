f414373018b18689c816fd7cbe9cc45e931e64cc
Mark Khromov <lane.71rus@gmail.com>
2018-08-23 21:27:37 +0300

    Added DefaultLogs class

---
+++ TheLog/DefaultLogs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TheLog.Providers;
+
+namespace TheLog {
+    public static class DefaultLogs {
+        static DefaultLogs() {
+            Console = Log<string, ConsoleColor>.Create(new ConsoleMessageProvider(), new ConsoleColorProvider());
+        }
+
+        public static Log<string, ConsoleColor> Console { get; }
+    }
+}
